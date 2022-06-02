3fb56d56ededfb4edc926e0039efed5e64c656ba
LauannyAMoreno <106778929+LauannyAMoreno@users.noreply.github.com>
2022-06-02 19:50:34 -0300

    Create aula-2022-06-02.c

---
+++ Exercícios_de_sala/aula-2022-06-02.c
@@ -0,0 +1,23 @@
+/*
+ler o valor do dia
+a saida do dia
+informa se o saldo e positivo ou negativo
+*/
+#include<stdio.h> 
+int main()
+{
+ float entradaseg, saidaseg,total diaseg;
+ printf("informe a entredaseg\n");
+ scanf("%f", &entradaseg);
+ printf("informe a saidaseg");
+ scanf("%f", &saidaseg);
+ total diaseg= entradaseg-saidaseg
+ printf("total diaseg:%f\n",total diaseg);
+ 
+ float entradater, saidater,total diater;
+ printf("informe a entredater\n");
+ scanf("%f", &entradaseg);
+ printf("informe a saidater");
+ scanf("%f", &saidater);
+ total diater= entradater-saidater
+ printf("total diater:%f\n",total diater);
